16c7ca8cde7f6a8887586e7fd987f044b83a62d8
1skendieur <iskendieur@gmail.com>
2025-07-02 17:24:56 +0200

    Initial commit: NBA All-Star Prediction Project

---
+++ .ipynb_checkpoints/api-checkpoint.py
@@ -0,0 +1,67 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
+import joblib
+import pandas as pd
+import numpy as np
+
+model = joblib.load("best_model_xgb.pkl")
+scaler = joblib.load("scaler.pkl")
+
+class PlayerFeatures(BaseModel):
+    Pos1: int
+    Age: float
+    Tm: int
+    G: float
+    GS: float
+    MP: float
+    FG: float
+    FGA: float
+    FG_dot: float = Field(alias="FG.")
+    X3P: float
+    X3PA: float
+    X3P_dot: float = Field(alias="X3P.")
+    X2P: float
+    X2PA: float
+    X2P_dot: float = Field(alias="X2P.")
+    eFG_dot: float = Field(alias="eFG.")
+    FT: float
+    FTA: float
+    FT_dot: float = Field(alias="FT.")
+    ORB: float
+    DRB: float
+    TRB: float
+    AST: float
+    STL: float
+    BLK: float
+    TOV: float
+    PF: float
+    PTS: float
+    Salary: float
+    mean_views: float
+    Conference: int
+    Role: int
+    Fvot: float
+    FRank: float
+    Pvot: float
+    PRank: float
+    Mvot: float
+    MRank: float
+    Score: float
+
+app = FastAPI()
+
+@app.post("/predict/")
+def predict(data: PlayerFeatures):
+    try:
+        input_df = pd.DataFrame([data.dict(by_alias=True)])
+        scaled_input = scaler.transform(input_df)
+        prob = model.predict_proba(scaled_input)[0][1]
+        prediction = "All-Star" if prob >= 0.5 else "Not All-Star"
+
+        return {
+            "all_star_probability": float(round(prob * 100, 2)),  # Преобразуем в обычный float
+            "prediction": prediction
+        }
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
---
+++ .ipynb_checkpoints/test-checkpoint.py
@@ -0,0 +1,16 @@
+import requests
+
+url = "http://127.0.0.1:8000/predict/"
+
+# Пример данных (ты можешь вставить реальные)
+sample_input = {
+    "Age": 28, "G": 75, "GS": 75, "MP": 34.5, "FG": 9.1, "FGA": 18.2, "FG_": 0.5,
+    "X3P": 2.3, "X3PA": 6.5, "X3P_": 0.35, "X2P": 6.8, "X2PA": 11.7, "X2P_": 0.58,
+    "eFG_": 0.57, "FT": 6.5, "FTA": 7.8, "FT_": 0.83, "ORB": 1.2, "DRB": 5.0,
+    "TRB": 6.2, "AST": 7.1, "STL": 1.3, "BLK": 0.4, "TOV": 3.2, "PF": 2.1,
+    "PTS": 27.0, "Salary": 30000000, "mean_views": 5.5,
+    "Pos1": 2, "Tm": 12, "Conference": 1, "Role": 0
+}
+
+response = requests.post(url, json=sample_input)
+print(response.json())
